5f039ceef5b31ba882b343b02f0e759031604ebe
Haiz Oppenheimer <haizop@gmail.com>
2016-12-16 14:30:36 -0500

    fizzbuzz p2

--- functions_2.exs
+++ functions_2.exs
@@ -1,6 +1,16 @@
-fizzbuzz = fn
-  (0, 0, _) -> "Fizz"
-  (0, _, _) -> "Buzz"
-  (_, 0, _) -> "FizzBuzz"
+fizzbuzzer = fn
+  (0, 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, 0, _) -> "Buzz"
   (_, _, c) -> c
 end
+
+fizzbuzz = fn (n) -> fizzbuzzer.(rem(n,3), rem(n,5), n) end
+
+fizzbuzz.(10)
+fizzbuzz.(11)
+fizzbuzz.(12)
+fizzbuzz.(13)
+fizzbuzz.(14)
+fizzbuzz.(15)
+fizzbuzz.(16)
