44f364c2467ad5b9013f1aae2cc24b304016d09c
Haiz Oppenheimer <haizop@gmail.com>
2017-01-08 16:04:01 -0500

    refactors

--- modules_functions_6_guess_num.exs
+++ modules_functions_6_guess_num.exs
@@ -1,20 +1,19 @@
 defmodule GuessNum do
-  def guess(actual, range) do
-    bottom..top = range
+  def guess(actual, bottom..top) do
     guess_num = div(top - bottom, 2) + bottom
     IO.puts("Is it #{guess_num}?")
     check(actual, guess_num, bottom, top)
   end
 
-  def check(actual, guess_num, bottom, top) when actual == guess_num do
+  def check(actual, guess_num, _bottom, _top) when actual == guess_num do
     IO.puts("Yes, it is #{actual}.")
   end
 
-  def check(actual, guess_num, bottom, top) when guess_num > actual do
+  def check(actual, guess_num, bottom, _top) when guess_num > actual do
     guess(actual, bottom..guess_num - 1)
   end
 
-  def check(actual, guess_num, bottom, top) when guess_num < actual do
+  def check(actual, guess_num, _bottom, top) when guess_num < actual do
     guess(actual, guess_num + 1..top)
   end
 end
