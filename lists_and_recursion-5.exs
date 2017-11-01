1e6694fb554648f7d3a3391c9b94952ccd65b3ef
Haiz Oppenheimer <haizop@gmail.com>
2017-10-31 22:21:55 -0400

    each, filter

--- lists_and_recursion-5.exs
+++ lists_and_recursion-5.exs
@@ -1,5 +1,17 @@
 defmodule MyList do
   def all?(list, func), do: false in _all?(list, func) == false
-  defp _all?([ head | [] ], func), do: [ func.(head) == true ]
-  defp _all?([ head | tail ], func), do: [ func.(head) == true | _all?(tail, func) ]
+  defp _all?([ h | [] ], func), do: [ func.(h) == true ]
+  defp _all?([ h | t ], func), do: [ func.(h) == true | _all?(t, func) ]
+
+  def each([ h | [] ], func), do: func.(h)
+  def each([ h | t ], func), do: (func.(h); each(t, func))
+
+  def filter([ h | [] ], func), do: if(func.(h) == true, do: h, else: [])
+  def filter([ h | t ], func) do
+    if func.(h) == true do
+      [ h | filter(t, func) ]
+    else
+      [ filter(t, func) ]
+    end
+  end
 end
